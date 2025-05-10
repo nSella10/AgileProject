6bf20d2d6a68143aaa653df9df37171da9c3fd01
Omri Peer <omripeer@h-MacBook-Pro-sl-Omri.local>
2025-05-10 12:30:29 +0300

    same domain

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -5,7 +5,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://guessify.com";
+    : "https://www.guessifyapp.com";
 
 export const USERS_URL = `${BASE_URL}/api/users`;
 

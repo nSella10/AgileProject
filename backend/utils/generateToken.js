0f057be55916f8252c6629070e91f8ab13be7a9a
Omri Peer <omripeer@h-MacBook-Pro-sl-Omri.local>
2025-05-10 12:55:20 +0300

    same domain

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,10 +6,12 @@ const generateToken = (res, userId) => {
     expiresIn: "30d",
   });
 
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "None",
+    secure: isProduction,
+    sameSite: isProduction ? "None" : "Lax", // ⚠️ כאן התיקון
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
